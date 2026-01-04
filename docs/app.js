0a6c6e6c50038574cb1a36690a89cca279052f09
jieun <kad1370@naver.com>
2026-01-04 19:55:38 +0900

    safari 환경에서 PWA 오디오 재생 되도록 수정

--- docs/app.js
+++ docs/app.js
@@ -17,7 +17,7 @@ fetch('./audios.json')
       const audioElement = div.querySelector('audio');
 
       audioElement.addEventListener('play', () => {
-        alert("현재 상태(readyState):", audioElement.readyState);
+        alert("현재 상태(readyState):" + audioElement.readyState);
 
         if (audioElement.readyState === 0) {
           audioElement.load();
@@ -26,7 +26,7 @@ fetch('./audios.json')
         audioElement.play().then(() => {
           console.log("재생 성공");
         }).catch(error => {
-          alert("재생 에러:", error.name);
+          alert("재생 에러:"+ error.name);
           // iOS PWA 세션 끊김 대응: 소스 재할당
           if (audioElement.readyState === 0) {
              audioElement.src = audioData.url;
@@ -36,8 +36,7 @@ fetch('./audios.json')
       });
 
       audioElement.onerror = () => {
-        const err = audioElement.error;
-        alert("오디오 에러 상세:", err);
+        alert("오디오 에러 상세:"+ audioElement.error);
       };
     });
   })
