bfdf40fd817e7160df3f929c1ea6b36419dcb2c9
jieun <kad1370@naver.com>
2026-01-04 19:14:30 +0900

    오디오 백그라운드 재생되도록 수정

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -12,6 +12,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // 오디오 파일 요청이면 서비스 워커가 개입하지 않고 통과시킴
+  if (e.request.destination === 'audio' || e.request.url.includes('.mp3')) {
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
   );
